939a89af7687b1408e571076c11ca0492812e969
Ammaar <fakirammaar8@gmail.com>
2020-06-19 14:48:46 +0200

    added js file and index file

---
+++ users.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function(){
+    // get a reference to the template script tag
+    var templateSource = document.querySelector(".userTemplate").innerHTML;
+
+    // compile the template
+    var userTemplate = Handlebars.compile(templateSource);
+    // pass the data into the template & get the HTML back
+    var userData = { users: [
+        {username: "alan", firstName: "Alan", lastName: "Johnson", email: "alan@test.com" },
+        {username: "allison", firstName: "Allison", lastName: "House", email: "allison@test.com" },
+        {username: "ryan", firstName: "Ryan", lastName: "Carson", email: "ryan@test.com" }
+      ]};
+    // get a reference to the element in the DOM where we would like to display the data
+   
+    var userDataElem = document.querySelector(".userData");
+    // put the resulting HTML into the target elements innerHTML
+    var userDataHTML = userTemplate(userData);
+    userDataElem.innerHTML = userDataHTML;
+});
