433890897817d5921a7316de5e13e821c151e1f5
Ammaar <fakirammaar8@gmail.com>
2020-06-22 12:59:10 +0200

    attempted using handlebars

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,40 +1,47 @@
-let radioBill=RadioBill();
+let radioBill = RadioBill();
 const radAddBtn = document.querySelector(".radioBillAddBtn");
 const callTotal2 = document.querySelector(".callTotalTwo");
 const smsTotal2 = document.querySelector(".smsTotalTwo");
 const total2 = document.querySelector(".totalTwo");
-const colorChanger = document.querySelector(".orange");
-
-
+const colorChanger = document.querySelector(".red");
+//var totalCostAll= document.querySelector(".totalCosts");
+var templateSource = document.querySelector(".costTemplate").innerHTML
 
 // compile the template
-var userTemplate = Handlebars.compile(template);
 
-
-
-
+var userTemplate = Handlebars.compile(templateSource);
 
 function calculateBtnClick() {
     // get a reference to the sms or call radio buttons
-    
-var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
+    // alert("bread")
+    var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
     var billItemType = checkedRadioBtn.value;
-        radioBill.tPhoneBill(billItemType);
-        callTotal2.innerHTML=radioBill.getCTotal();
-    smsTotal2.innerHTML=radioBill.getSTotal();
-        total2.innerHTML = radioBill.getTotal();
-    stylingTotalApplication();
-        ;
-    }
-    function stylingTotalApplication() {
+    radioBill.tPhoneBill(billItemType);
+    //   var prices={cost:[{smsTotalAmount :radioBill.getSTotal(),callTotalAmount:radioBill.getCTotal(),totalAmount:radioBill.getTotal()}]}
+    //var priceStore=userTemplate(prices)
+    // totalCostAll.innerHTML= priceStore;
 
-        colorChanger.classList.remove("danger");
-        colorChanger.classList.remove("warning");
-          colorChanger.classList.add(radioBill.stylingTotal());
-    }
-    
+     templateSource={smsTotalAmount :radioBill.getSTotal(),callTotalAmount:radioBill.getCTotal(),totalAmount:radioBill.getTotal()}
 
-// radAddBtn.addEventListener("click", calculateBtnClick);
+
+    // var userDataHTML = userTemplate(prices);
+
+    // totalValues.innerHTML = userDataHTML;
+    //     callTotal2.innerHTML=radioBill.getCTotal();
+    // smsTotal2.innerHTML=radioBill.getSTotal();
+    //     total2.innerHTML = radioBill.getTotal();
+    // stylingTotalApplication();
+
+}
+// function stylingTotalApplication() {
+
+//     colorChanger.classList.remove("danger");
+//     colorChanger.classList.remove("warning");
+//       colorChanger.classList.add(radioBill.stylingTotal());
+// }
+
+
+radAddBtn.addEventListener("click", calculateBtnClick);
 //add an event listener for when the add button is pressed
 
 //in the event listener get the value from the billItemTypeRadio radio buttons
