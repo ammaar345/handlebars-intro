5344765e1fd0c43d124654221260f706051e82d1
Ammaar <fakirammaar8@gmail.com>
2020-06-29 12:29:36 +0200

    added another folder

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,13 +1,13 @@
 // get a reference to the textbox where the bill type is to be entered
-let textBill=TextBill();
-const enterTxt=document.querySelector(".billTypeText");
+let textBill = TextBill();
+const enterTxt = document.querySelector(".billTypeText");
 //get a reference to the add button
-const addBtn=document.querySelector(".addToBillBtn");
+const addBtn = document.querySelector(".addToBillBtn");
 //create a variable that will keep track of the total bill
-const callTotalElement=document.querySelector(".callTotalOne");
-const smsTotalElement=document.querySelector(".smsTotalOne");
-const total=document.querySelector(".totalOne");
-const color=document.querySelector(".red");
+const callTotalElement = document.querySelector(".callTotalOne");
+const smsTotalElement = document.querySelector(".smsTotalOne");
+const total = document.querySelector(".totalOne");
+const color = document.querySelector(".red");
 
 
 // var templateSource2 = document.querySelector(".costTemplate").innerHTML;
@@ -16,22 +16,22 @@ const color=document.querySelector(".red");
 // //compile template 
 // var userTemplate2 = Handlebars.compile(templateSource2);
 
-function calculateBtnClicked(){
-  var billSt=enterTxt.value;
+function calculateBtnClicked() {
+  var billSt = enterTxt.value;
   textBill.totPhoneBill(billSt)
   // callTotalElement.innerHTML=textBill.getTotalCallPrice();;
   //  smsTotalElement.innerHTML=textBill.getTotalSmsPrice();
   //  total.innerHTML = textBill.getTotalPrice();;
-  var result2 = { smsTotalAmount:textBill.getTotalSmsPrice(), callTotalAmount: textBill.getTotalCallPrice(), totalAmount: textBill.getTotalPrice() }
-    pricesAll2.innerHTML = userTemplate(result2)
-   styledApplic()
+  var result2 = { smsTotalAmount: textBill.getTotalSmsPrice(), callTotalAmount: textBill.getTotalCallPrice(), totalAmount: textBill.getTotalPrice() }
+  pricesAll2.innerHTML = userTemplate(result2)
+  styledApplic()
 
 }
-function styledApplic(){
-  
+function styledApplic() {
+
   color.classList.remove("danger");
   color.classList.remove("warning");
-color.classList.add(textBill.styledTotal())
+  color.classList.add(textBill.styledTotal())
 }
 
-       addBtn.addEventListener("click",calculateBtnClicked);
+addBtn.addEventListener("click", calculateBtnClicked);
